a1208657610455f515d9ad870ec3fa59f61955fd
Scott Olson <scottolsonjr@users.noreply.github.com>
2020-02-13 11:48:43 -0800

    Allow for separate ODBC connections based on Facility

--- RS.ScriptLinkDemo.CSharp.Soap.Tests/Factories/ConnectionStringSelectorTests.cs
+++ RS.ScriptLinkDemo.CSharp.Soap.Tests/Factories/ConnectionStringSelectorTests.cs
@@ -7,42 +7,90 @@ namespace RS.ScriptLinkDemo.CSharp.Soap.Tests.Factories
     public class ConnectionStringSelectorTests
     {
         [TestMethod]
-        public void GetConnectionStringName_ReturnsAVPM()
+        public void GetConnectionStringName_Facility1_ReturnsAVPM()
         {
             // Arrange
             string namespaceName = "AVPM";
+            string facility = "1";
             string expected = "ODBCAVPM";
 
             // Act
-            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName);
+            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName, facility);
 
             // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void GetConnectionStringName_ReturnsAVCWS()
+        public void GetConnectionStringName_Facility1_ReturnsAVCWS()
         {
             // Arrange
             string namespaceName = "AVCWS";
+            string facility = "1";
             string expected = "ODBCAVCWS";
 
             // Act
-            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName);
+            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName, facility);
 
             // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void GetConnectionStringName_ReturnsAVMSO()
+        public void GetConnectionStringName_Facility1_ReturnsAVMSO()
         {
             // Arrange
             string namespaceName = "AVMSO";
+            string facility = "1";
             string expected = "ODBCAVMSO";
 
             // Act
-            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName);
+            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName, facility);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetConnectionStringName_Facility2_ReturnsAVPM()
+        {
+            // Arrange
+            string namespaceName = "AVPM";
+            string facility = "2";
+            string expected = "ODBCAVPM2";
+
+            // Act
+            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName, facility);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetConnectionStringName_Facility2_ReturnsAVCWS()
+        {
+            // Arrange
+            string namespaceName = "AVCWS";
+            string facility = "2";
+            string expected = "ODBCAVCWS2";
+
+            // Act
+            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName, facility);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetConnectionStringName_Facility2_ReturnsAVMSO()
+        {
+            // Arrange
+            string namespaceName = "AVMSO";
+            string facility = "2";
+            string expected = "ODBCAVMSO2";
+
+            // Act
+            string actual = ConnectionStringSelector.GetConnectionStringName(namespaceName, facility);
 
             // Assert
             Assert.AreEqual(expected, actual);
--- RS.ScriptLinkDemo.CSharp.Soap/Factories/ConnectionStringSelector.cs
+++ RS.ScriptLinkDemo.CSharp.Soap/Factories/ConnectionStringSelector.cs
@@ -8,25 +8,43 @@ namespace RS.ScriptLinkDemo.CSharp.Soap.Factories
     {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// Create connection string collection.
+        /// <para>Use this when you have a single facility or the one account can access all required facilities.</para>
+        /// </summary>
+        /// <returns></returns>
         public static ConnectionStringCollection GetConnectionStringCollection()
         {
-            string pmString = GetConnectionString("AVPM");
-            string cwsString = GetConnectionString("AVCWS");
-            string msoString = GetConnectionString("AVMSO");
+            return GetConnectionStringCollection("1");
+        }
+        /// <summary>
+        /// Create connection string collection based on provided facility.
+        /// </summary>
+        /// <param name="facility">Facility number from OptionObject.</param>
+        /// <returns></returns>
+        public static ConnectionStringCollection GetConnectionStringCollection(string facility)
+        {
+            string pmString = GetConnectionString("AVPM", facility);
+            string cwsString = GetConnectionString("AVCWS", facility);
+            string msoString = GetConnectionString("AVMSO", facility);
             return new ConnectionStringCollection(pmString, cwsString, msoString);
         }
 
-        public static string GetConnectionString(string namespaceName)
+        public static string GetConnectionString(string namespaceName, string facility)
         {
-            string connectionName = GetConnectionStringName(namespaceName);
+            string connectionName = GetConnectionStringName(namespaceName, facility);
             if (connectionName != null && ConfigurationManager.ConnectionStrings[connectionName] != null)
                 return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
             return "";
         }
 
-        public static string GetConnectionStringName(string namespaceName)
+        public static string GetConnectionStringName(string namespaceName, string facility)
         {
-            return "ODBC" + namespaceName;
+            string connectionStringName = "ODBC" + namespaceName;
+            if (facility != "1")
+                connectionStringName += facility;
+            logger.Debug("Selected Connection String Name {connectionStringName}.", connectionStringName);
+            return connectionStringName;
         }
     }
 }
